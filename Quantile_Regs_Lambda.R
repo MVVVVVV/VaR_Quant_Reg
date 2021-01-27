47a33f48bbfc0728bc43470ac90cc1f8ed02832e
MVVVVVV <76630830+MVVVVVV@users.noreply.github.com>
2021-01-27 19:27:18 +0100

    caviar + quant regs

---
+++ Quantile_Regs_Lambda.R
@@ -0,0 +1,202 @@
+library(rqPen)
+library(hqreg)
+library(data.table)
+library(tibble)
+
+var_quantile = 0.01
+max_sum_betas = 1
+rolling_windows = c(250,500,1000,1500, "ERW")
+start_date = "2007-01-03"
+end_date = "2014-12-31"
+my_path = "C:\\Users\\MV\\Desktop\\Projet Quant\\"
+  
+rdt <- read.csv(paste(my_path,"rdt.csv", sep=""), header=TRUE, row.names="Date")
+#%%%%%%%%%%%%%
+# var <- read csv
+
+start_index = grep(start_date, row.names(rdt))
+end_index = grep(end_date, row.names(rdt))
+nbr_prediction = end_index - start_index +1
+
+var_prediction <- data.frame(matrix(ncol = 8, nrow = nbr_prediction))
+var_col_names <- c("Date","lasso_bic", "ridge_cv", "elastic_cv", "lasso_cv",
+                   "ridge_fix", "elastic_fix", 'lasso_fix')
+colnames(var_prediction) <- var_col_names
+n = 1
+
+# on crée nos lambdas sur echelle de 10
+log_values <- cbind(0.1,1,10,100,1000,10000,100000)
+nb_steps = 13
+lambdas = seq(0,1,1/nb_steps)*0.01
+
+for (i in 1:length(log_values)){
+  temp = seq(0,1,1/(nb_steps))*log_values[i]
+  lambdas = append(lambdas, temp[2:length(temp)])
+}
+
+# on itere sur nos 4 rolling windows
+for (RW in rolling_windows){
+  index_RW = 1
+  # cette matrice comprendra les tick loss par actifs par lambdas pour chaque modele
+  tick_loss_per_lambda_lasso  = matrix(,nrow=30,ncol = length(lambdas))
+  tick_loss_per_lambda_ridge = matrix(,nrow=30,ncol = length(lambdas))
+  tick_loss_per_lambda_elastic = matrix(,nrow=30,ncol = length(lambdas))
+  
+  # ces matrices comprendront la moyenne des tick loss des 30 actifs par fenetre par lambda
+  tick_loss_per_lambda_lasso_per_RW = matrix(,nrow=length(rolling_windows),ncol = length(lambdas))
+  tick_loss_per_lambda_ridge_per_RW = matrix(,nrow=length(rolling_windows),ncol = length(lambdas))
+  tick_loss_per_lambda_elastic_per_RW = matrix(,nrow=length(rolling_windows),ncol = length(lambdas))
+  
+  # les predictions pour chaque actif pour chaque date
+  var_prediction_all_asset_lasso_bic = matrix(, nrow=nbr_prediction, ncol = 30)
+  var_prediction_all_asset_ridge_cv = matrix(, nrow=nbr_prediction, ncol = 30)
+  var_prediction_all_asset_elastic_cv = matrix(, nrow=nbr_prediction, ncol = 30)
+  var_prediction_all_asset_lasso_bic = matrix(, nrow=nbr_prediction, ncol = 30)
+  var_prediction_all_asset_ridge_fix = matrix(, nrow=nbr_prediction, ncol = 30)
+  var_prediction_all_asset_elastic_fix = matrix(, nrow=nbr_prediction, ncol = 30)
+  var_prediction_all_asset_lasso_bic = matrix(, nrow=nbr_prediction, ncol = 30)
+  asset_index = 1
+  for(asset in colnames(rdt)){
+    
+    # les matrices var_predictions comprennent nos prévisions pour cet actif
+    # avec en row les dates et en col les differents lambdas
+    var_prediction_lasso = matrix(,nrow = nbr_prediction, ncol = length(lambdas))
+    var_prediction_ridge = matrix(,nrow = nbr_prediction, ncol = length(lambdas))
+    var_prediction_elastic = matrix(,nrow =nbr_prediction, ncol = length(lambdas))
+    lasso_bic = matrix(,nrow = nbr_prediction, ncol = 30)
+    ridge_cv =matrix(,nrow = nbr_prediction, ncol = 30)
+    elastic_cv =matrix(,nrow = nbr_prediction, ncol = 30)
+    lasso_cv =matrix(,nrow = nbr_prediction, ncol = 30) 
+    ridge_fix =matrix(,nrow = nbr_prediction, ncol = 30)
+    elastic_fix =matrix(,nrow = nbr_prediction, ncol = 30)
+    lasso_fix =matrix(,nrow = nbr_prediction, ncol = 30)
+    # la matrice qui va contenir les predictions pour les differents QR par dates
+    var_prediction_asset = matrix(,nrow=nbr_prediction, ncol = 7)
+    
+    for (date_row in seq(start_index,end_index, by=1)){
+      start_rol_win = date_row-RW-1
+      if (RW ="ERW"){
+        start_rol_win = 1
+      }
+      end_rol_win = date_row-1
+      my_rdt = rdt[start_rol_win:end_rol_win, asset]
+      my_var = var[start_rol_win:end_rol_win, asset]
+      fit_lasso = hqreg(my_rdt, my_var, method = "quantile", tau = var_quantile, alpha = 1, 
+                        lambda = lambdas)
+      fit_ridge = hqreg(my_rdt, my_var, method = "quantile", tau = var_quantile, alpha = 0,
+                        lambda = lambdas)
+      fit_elastic = hqreg(my_rdt, my_var, method = "quantile", tau = var_quantile, alpha = 0.5, 
+                          lambda = lambdas)
+      
+      var_prediction_lasso[date_row,] = predict(fit_lasso, var[date_row,asset],
+                                     lambda = lambdas)
+      var_prediction_ridge[date_row,] = predict(fit_ridge, var[date_row,asset],
+                                     lambda = lambdas)
+      var_prediction_elastic[date_row,] = predict(fit_elastic, var[date_row,asset],
+                                     lambda = lambdas)
+      
+     
+    }
+    
+ 
+    tick_loss_per_lamdbda_lasso[asset_index,] =  tick_loss(rdt, var_prediction_lasso, var_quantile)
+    tick_loss_per_lambda_ridge[asset_index,] = tick_loss(rdt, var_prediction_ridge, var_quantile)
+    tick_loss_per_lambda_elastic[asset_index,] = tick_loss(rdt, var_prediction_elastic, var_quantile)
+    
+    # on selectionne le meilleure lambda en fct des differents criteres
+    index_lasso_bic =which.min(BIC(my_rdt, var_prediction_lasso, var_quantile,
+                                  fit_lasso$beta))
+    ridge_cv =11
+    elastic_cv =1
+    lasso_cv =1 
+    ridge_fix =heuristic(lambda=fit_ridge$lambda,
+                         betas=fit_ridge$beta[2:.N,] )
+    elastic_fix =heuristic(lambda=fit_elastic$lambda,
+                           betas=fit_elastic$beta[2:.N,] )
+    lasso_fix =heuristic(lambda=fit_lasso$lambda,
+                         betas=fit_lasso$beta[2:.N,] )
+    
+    
+    # on ajoute l'asset pour chaque modele
+    var_prediction_all_asset_lasso_bic[, asset_index] = var_prediction_lasso[,index_min_bic]
+    var_prediction_all_asset_ridge_cv[, asset_index] = var_prediction_asset[,"ridge_cv"]
+    var_prediction_all_asset_elastic_cv[, asset_index] = var_prediction_asset[,"elastic_cv"]
+    var_prediction_all_asset_lasso_cv[, asset_index] = var_prediction_asset[,"lasso_cv"]
+    var_prediction_all_asset_ridge_fix[, asset_index] = var_prediction_asset[,"ridge_fix"]
+    var_prediction_all_asset_elastic_fix[, asset_index] = var_prediction_asset[,"elastic_fix"]
+    var_prediction_all_asset_lasso_fix[, asset_index] = var_prediction_asset[,"lasso_fix"]
+    
+    asset_index = asset_index + 1
+  }
+  
+  tick_loss_per_lambda_lasso_per_RW[index_RW,] = colMeans(tick_loss_per_lambda_lasso)
+  tick_loss_per_lambda_ridge_per_RW[index_RW,] = colMeans(tick_loss_per_lambda_ridge)
+  tick_loss_per_lambda_elastic_per_RW[index_RW,] = colMeans(tick_loss_per_lambda_elastic)
+  
+  
+  write.csv(var_prediction_all_asset_lasso_bic, paste(my_path,"Lasso_bic_",RW,"RW.csv"))
+  write.csv(var_prediction_all_asset_ridge_cv, paste(my_path,"Ridge_cv_",RW,"RW.csv"))
+  write.csv(var_prediction_all_asset_elastic_cv, paste(my_path,"ELastic_cv_",RW,"RW.csv"))
+  write.csv(var_prediction_all_asset_lasso_cv, paste(my_path,"Lasso_cv_",RW,"RW.csv"))
+  write.csv(var_prediction_all_asset_ridge_fix, paste(my_path,"Ridge_fix_",RW,"RW.csv"))
+  write.csv(var_prediction_all_asset_elastic_fix, paste(my_path,"Elastic_fix_",RW,"RW.csv"))
+  write.csv(var_prediction_all_asset_lasso_fix, paste(my_path,"Lasso_fix_",RW,"RW.csv"))
+  
+  index_RW = index_RW + 1
+}
+
+write.csv(tick_loss_per_lambda_lasso_per_RW, paste(my_path,"avg_tick_loss_lasso"))
+write.csv(tick_loss_per_lambda_ridge_per_RW, paste(my_path,"avg_tick_loss_ridge"))
+write.csv(tick_loss_per_lambda_elastic_per_RW, paste(my_path,"avg_tick_loss_elastic"))
+
+
+tick_loss<-function(rdt, var, var_quantile){
+  hit = var_quantile*sweep(rdt,-var,1, FUN =  "<") * sweep(rdt, var,1, FUN = "-")
+  result = (t(hit) %*% sweep(rdt,var,1,FUN = "-"))/t
+  
+  return (result)
+}
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# on utilise bic a minimiser en fct de lambda
+# valable pour le lasso
+BIC <-function(rdt, var, alpha, betas){
+  t <- size(rdt)
+  df <- sum(betas!=0)
+  result <- log(tick_loss(rdt, var, alpha)) + (log(t)/2*t)*df
+  return (result)
+}
+
+
+#%%%%%%%%%%%%%%%%%%%%
+# Cross Validation
+cross_validation<-function(alpha, lambda, delta, rdt, rho, qc){
+
+}
+
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#heuristic rule
+heuristic<-function(lambda, betas){
+  s = 0.8
+  max_lambda = 0
+  for (i in length(lambda)){
+    if (rowSums(betas)[i]<0.8 & max_lambda < lambda[i] ){
+      max_lambda = lambda[i]
+      max_index = i
+    }
+  }
+  #result<- list(max_lambda, max_index)
+  return(max_index)
+}
+
+
+X = matrix(rnorm(1000*100), 1000, 100)
+beta = rnorm(10)
+eps = 4*rnorm(1000)
+y = drop(X[,1:10] %*% beta + eps)# Huber loss
+fit2 = hqreg(X, y, method = "quantile", tau = 0.2)
+
+
+
+
--- caviar.py
+++ caviar.py
@@ -1,32 +1,33 @@
 import numpy as np
 from scipy.optimize import minimize
+import pandas as pd
+import itertools
+import time
+from numba import jit
+import math
 
 
-# https://github.com/Buczman/CaviaR
-# modifié et traduit du R
-
-
-def SAV(beta, rdt, var_start, var_predict):
+@jit(nopython=True)
+def SAV(beta, rdt, var, var_predict):
     """
     Symmetric absolute value
     """
-    var = np.array([var_start])
     size = rdt.size
 
     for i in range(1, size):
         var[i] = beta[0] + beta[1] * var[i - 1] + beta[2] * abs(rdt[i - 1])
 
     if var_predict:
-        var[size] = beta[0] + beta[1] * var[size] + beta[2] * abs(rdt[size - 1])
+        var[size] = beta[0] + beta[1] * var[size - 1] + beta[2] * abs(rdt[size - 1])
 
     return var
 
 
-def AS(beta, rdt, var_start, var_predict):
+@jit(nopython=True)
+def AS(beta, rdt, var, var_predict):
     """
     Asymmetric slope
     """
-    var = np.array([var_start])
     size = rdt.size
 
     for i in range(1, size):
@@ -39,11 +40,11 @@ def AS(beta, rdt, var_start, var_predict):
     return var
 
 
-def IG(beta, rdt, var_start, var_predict):
+@jit(nopython=True)
+def IG(beta, rdt, var, var_predict):
     """
     Indirect GARCH(1; 1)
     """
-    var = np.array([var_start])
     size = rdt.size
 
     for i in range(1, size):
@@ -55,25 +56,25 @@ def IG(beta, rdt, var_start, var_predict):
     return var
 
 
-def AD(beta, rdt, var_start, var_predict, var_quantile=0.05, G=10):
+@jit(nopython=True)
+def AD(beta, rdt, var, var_predict, var_quantile=0.01, G=10):
     """
     Adaptive
     G is by default set to 10 as in Engle and Manganelli (2004)
     """
-    var = np.array([var_start])
     size = rdt.size
 
     for i in range(1, size):
-        var[i] = var[i - 1] + beta[0] * (1 / (1 + np.exp(G * (rdt[i - 1] - var[i - 1]))) - var_quantile)
+        var[i] = var[i - 1] + beta[0] * ((1 + np.exp(G * (rdt[i - 1] - var[i - 1]))) ** -1 - var_quantile)
         # var_quantile etre sur??
 
     if var_predict:
-        var[size] = var[size - 1] + beta[0] * (1 / (1 + np.exp(G * (rdt[size - 1] - var[size - 1]))) - var_quantile)
+        var[size] = var[size - 1] + beta[0] * ((1 + np.exp(G * (rdt[size - 1] - var[size - 1]))) ** -1 - var_quantile)
     return var
 
 
-def CaviarOptim(rdt, var_quantile=0.05, model=1):
-    emp_var = np.quantile(rdt[1:300], var_quantile)  # le papier recommande de prendre les 300 premieres
+def CaviarOptim(rdt, var_quantile=0.01, model=1):
+    emp_var = np.quantile(rdt[0:300], var_quantile)  # le papier recommande de prendre les 300 premieres
     size = np.size(rdt)
     var = np.zeros(size)
     hit = np.zeros(size)
@@ -81,49 +82,63 @@ def CaviarOptim(rdt, var_quantile=0.05, model=1):
     # on initialise nos betas
     if model == 1 or model == 3:
         n_beta = 3
-        n_initialcond = 10
     elif model == 2:
         n_beta = 4
-        n_initialcond = 15
     elif model == 4:
         n_beta = 1
-        n_initialcond = 5
 
-    # on crée plusieurs conditions initiales, on selectionnera le meilleur resultat plus tard
-    params_start = np.random.random((n_initialcond, n_beta))
-    res_opt = np.zeros(n_initialcond, n_beta + 1)
+    n = 0
+    while n < 4:
+        n += 1
+        # on crée plusieurs conditions initiales, on selectionnera le meilleur resultat plus tard
+        params_start = np.random.random(n_beta)
+        res_opt = np.zeros(n_beta + 1)
 
-    for i in range(1, n_initialcond + 1):
-        args = [1, model, rdt, var_quantile, emp_var]
-        res = minimize(fun=ObjectiveFunction, x0=params_start[i, :], args=args)
-        res_opt[i, 1] = res.fun
-        res_opt[i, 2:(n_beta + 1)] = np.array(res.x)
+        args = (1, model, rdt, var_quantile, emp_var)
+        bnds = list(itertools.repeat((-2.00001, 2.000001), n_beta))
+        res = minimize(fun=ObjectiveFunction, x0=params_start, args=args, method='Nelder-Mead')
+        if model == 3:
+            res = minimize(fun=ObjectiveFunction, x0=res.x, args=args, bounds=bnds)
+        else:
+            res = minimize(fun=ObjectiveFunction, x0=res.x, args=args, method='BFGS')
+
+        fun_value = res.fun
+        best_betas = res.x
+
+        var = ObjectiveFunction(best_betas, 2, model, rdt, var_quantile, emp_var, True)
+        var_predict = var[-1]
+        var = var[:-1]
+
+        if not math.isnan(var_predict):
+            break
+
+    return fun_value, best_betas, var, -var_predict
 
 
 def ObjectiveFunction(beta, out, model, rdt, var_quantile, emp_quantile, var_predict=False):
     if var_predict:
-        var = np.zeros(0, np.size(rdt) + 1)
+        var = np.zeros((np.size(rdt) + 1))
     else:
-        var = np.zeros(0, np.size(rdt))
+        var = np.zeros(np.size(rdt))
         hit = var
 
-    var[0] = - emp_quantile  # pourquoi le -??? a retirer prob
+    var[0] = -emp_quantile
 
-    if model == "SAV":
-        var = SAV(beta, rdt, var[0], var_predict)
-    elif model == "AS":
-        var = AS(beta, rdt, var[0], var_predict)
-    elif model == "IG":
-        var = IG(beta, rdt, var[0], var_predict)
-    elif model == "AD":
-        var = AD(beta, rdt, var[0], var_predict)
+    if model == 1:
+        var = SAV(beta, rdt, var, var_predict)
+    elif model == 2:
+        var = AS(beta, rdt, var, var_predict)
+    elif model == 3:
+        var = IG(beta, rdt, var, var_predict)
+    elif model == 4:
+        var = AD(beta, rdt, var, var_predict)
 
     if not var_predict:
         hit = (rdt < -var) - var_quantile
         if out == 1:
-            RQ = -1 * np.transpose(hit) * (rdt + var)
-            if RQ > 10 ** 10:
-                RQ = 10 ** 10
+            RQ = -1 * np.dot(np.transpose(hit), (rdt + var))
+            if RQ == float("inf"):
+                RQ = 100000000000
 
             return RQ
 
@@ -132,3 +147,27 @@ def ObjectiveFunction(beta, out, model, rdt, var_quantile, emp_quantile, var_pre
 
     elif var_predict:
         return var
+
+
+if __name__ == '__main__':
+    rdt = pd.read_csv(r'C:\Users\MV\Desktop\Projet Quant\rdt.csv', index_col=0)
+    start_date = '2000-01-03'
+    end_date = '2006-12-29'
+    start_index = list(rdt.index).index(start_date)
+    end_index = list(rdt.index).index(end_date)
+    var_prediction = pd.DataFrame(columns=rdt.columns, index=rdt.index[start_index:end_index + 1])
+
+    for model in [1,2,3,4]:
+        for column in rdt:
+            print(column)
+            i = 0
+            t0 = time.time()
+            for date in rdt.index[start_index - 1:end_index]:
+                my_rdt = rdt.loc[:date][column].tail(1000)
+                my_rdt = np.squeeze(np.asarray(my_rdt))  # utilisable par numba
+                fun_value, best_betas, var, var_prediction[column][i] = CaviarOptim(my_rdt, model=model)
+                i += 1
+            t1 = time.time()
+            print(t1 - t0)
+        var_prediction.fillna(method='ffill')
+        var_prediction.to_csv(r'C:\Users\MV\Desktop\Projet Quant\caviar_' + str(model) + '_NM_BFGS_V3_2000.csv')
